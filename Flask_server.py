714e6c724f6dbc074c4d404c7defa560080759f7
BRUCE-ARHIN SHADRACH <134882896+shadrach098@users.noreply.github.com>
2024-12-02 22:11:23 -0500

    Add files via upload

---
+++ Flask_server.py
@@ -0,0 +1,55 @@
+from flask import Flask, render_template, request,redirect
+import joblib
+import re
+import nltk
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+
+Fake_news = Flask(__name__,template_folder='./templates',static_folder='./static')
+
+vector = joblib.load(r"Models\TfidfVectorizer.pkl")
+loaded_model = joblib.load(r"Models\PassiveAggressiveClassifier.pkl")
+lemmatizer = WordNetLemmatizer()
+stpwrds = set(stopwords.words('english'))
+corpus = []
+
+def fake_news_det(news):
+    review = news
+    review = re.sub(r'[^a-zA-Z\s]', '', review)
+    review = review.lower()
+    review = nltk.word_tokenize(review)
+    corpus = []
+    for y in review :
+        if y not in stpwrds :
+            corpus.append(lemmatizer.lemmatize(y))
+    input_data = [' '.join(corpus)]
+    vectorized_input_data = vector.transform(input_data)
+    prediction = loaded_model.predict(vectorized_input_data)
+     
+    return prediction
+
+
+
+@Fake_news.route('/',methods=['GET'])
+def home():
+    return redirect("http://localhost:8502")
+    
+    
+@Fake_news.route('/Model_prediction',methods=["POST"])
+def predict():
+    Data=request.get_json()
+    text=Data.get('txt',{})
+    if text:
+        pred=fake_news_det(text)
+        if pred[0] == 1:
+            res="Prediction of the News :  Looking Fake News📰"
+        else:
+            res="Prediction of the News : Looking Real News📰 "
+    else:
+        return "Input is empty",404        
+    return res
+     
+    
+if __name__ =="__main__":
+    Fake_news.run('0.0.0.0',5000)    
+    
---
+++ GUI.py
@@ -0,0 +1,24 @@
+import streamlit as st,requests
+with st.sidebar:
+    st.title("ABOUT 👇")
+    st.write("Fake news detection using machine learning is a project that involves training a machine learning model to distinguish between real and fake news articles. The model is trained on a dataset of news articles using supervised learning algorithms and NLP techniques. The goal of the project is to help identify and flag fake news articles to prevent their spread and reduce the impact of misinformation in society.")
+    st.divider()
+    st.title("Created By: ")
+    st.write(":rainbow[Bruce-Arhin Shadrach  & Appiah Derick]")
+    
+    
+
+st.header(":red[FAKE NEWS DETECTION USING  PASSIVE AGGRESSIVE CLASSIFIER ]")
+st.divider()
+
+if text:=st.chat_input("Enter news headlines "):
+    
+        try:
+            url="http://127.0.0.1:5000/Model_prediction"
+            with st.spinner():
+                proced=requests.post(url,json={'txt':text})
+                if proced.status_code==200:
+                    with st.chat_message('assistant'):
+                        st.markdown(proced.text)
+        except Exception as ex:
+            st.warning(f":red[Server Error occured, Please check your internet connection and try again]", icon="⚠️")    
